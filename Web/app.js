c2711d0f4a93ae40972caea633ebfaef8a44cbc3
Joe661 <1585358813@qq.com>
2020-11-24 11:04:57 +0800

    front-end

---
+++ Web/app.js
@@ -0,0 +1,38 @@
+﻿var app = angular.module('PackageApp', ['ngRoute']);
+
+app.controller('UplaodController', function ($scope, $route) { $scope.$route = $route; })
+app.controller('InExShowController', function ($scope, $route) { $scope.$route = $route; })
+app.config(['$locationProvider', function ($locationProvider) {
+    $locationProvider.hashPrefix('');
+}]);
+
+app.config(function ($routeProvider) {
+    $routeProvider.
+        when('/default', {
+            templateUrl: 'default.html',
+            controller: 'UplaodController'
+        }).
+        when('/inexshow', {
+            templateUrl: 'inexshow.html',
+            controller: 'InExShowController'
+        }).
+        otherwise({
+            redirectTo: '/default'
+        });
+});
+app.controller("AppController", function ($scope, $http) {
+    // 父级接收  
+    $scope.$on('page', function (event, data) {
+        $scope.page = data;
+    });
+
+    $scope.GetMenuList = function () {
+        $http.get("/api/App/GetMenuList").then(
+            function (data) {
+                $scope.menulist = data.data.list;
+        })
+    }
+
+    $scope.GetMenuList();
+})
+ 
